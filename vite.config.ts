3dc493612ab9fa4856456d34af696d3c91e8004a
24im0147 <24im0147@i-u.ac.jp>
2026-04-10 01:12:23 +0000

    fix: add base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/my-vite-app/', // ← この行を必ず追加してください！
   plugins: [
     react(),
     tailwindcss(),

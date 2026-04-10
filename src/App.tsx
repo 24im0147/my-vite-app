28749c0dabd059780c5b1d6ee4694d9e85736886
24im0147 <24im0147@i-u.ac.jp>
2026-04-10 00:50:57 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-700 text-white h-screen flex items-center justify-center text-3xl font-bold">
+      こんにちは
+    </div>
+  )
+}
+
+export default App
